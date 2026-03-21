65835d44929611095fada6a09a53dd1f4a9683d4
Mahammad Safaf <mahammadsafaf999@gmail.com>
2026-03-22 00:05:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins + Docker CI/CD 🚀"
+    return """
+    <h1>🚀 CI/CD Pipeline Working!</h1>
+    <p>Jenkins + Docker + GitHub Integration Successful</p>
+    """
 
-if __name__ == '__main__':
+@app.route('/health')
+def health():
+    return "App is running ✅"
+
+@app.route('/version')
+def version():
+    return "Version 1.0 - Deployed via Jenkins CI/CD"
+
+if _name_ == '_main_':
     app.run(host='0.0.0.0', port=5000)
